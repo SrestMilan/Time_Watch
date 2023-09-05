b590630f5a1c907bcf1c0ab2eaff87c425d0f345
SrestMilan <shrestham780@gmail.com>
2023-09-05 18:25:06 +0545

    completed project

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ let second=document.getElementById("sec");
 // assigning regular function in time variable
 let time=function getTime(){
     let currentTime=new Date();
-    hour.innerHTML=currentTime.getHours();
-    minute.innerHTML=currentTime.getMinutes();
-    second.innerHTML=currentTime.getSeconds();
+    hour.innerHTML=(currentTime.getHours()<10?"0":"")+currentTime.getHours();
+    minute.innerHTML=(currentTime.getMinutes()<10?"0":"")+currentTime.getMinutes();
+    second.innerHTML=(currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
+
 }
 // set interval for time of 1000 milliseconds=1 second
 setInterval(time,1000);
